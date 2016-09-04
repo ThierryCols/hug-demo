9fa037a6798f0186888dfd41af6fd41c983ae778
ThierryCols <thierrycolsenet@gmail.com>
2016-09-04 11:17:06 +0200

    initial commit. False data in memory and basic functions

---
+++ demo_data.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+
+
+users = [
+    {
+        'id': 1,
+        'name': 'John',
+    }, {
+        'id': 2,
+        'name': 'Paul',
+    }, {
+        'id': 3,
+        'name': 'Marc',
+}]
+
+pronos = [{
+    'id': 1,
+    'gameId': 1,
+    'userId': 1,
+    'teamId1': 2,
+    'teamId2': 1,
+    'winner': 1,
+}, {
+    'id': 2,
+    'gameId': 1,
+    'userId': 3,
+    'teamId1': 3,
+    'teamId2': 0,
+    'winner': 1,
+}, {
+    'id': 3,
+    'gameId': 1,
+    'userId': 2,
+    'teamId1': 2,
+    'teamId2': 3,
+    'winner': 2,
+}, {
+    'id': 4,
+    'gameId': 3,
+    'userId': 2,
+    'teamId1': 1,
+    'teamId2': 2,
+    'winner': 2,
+}, {
+    'id': 5,
+    'gameId': 3,
+    'userId': 1,
+    'teamId1': 2,
+    'teamId2': 1,
+    'winner': 1,
+}]
+games = [{
+    'id': 1,
+    'teamId1': 1,
+    'teamId2': 2,
+    'gameDate': datetime(2016, 6, 15),
+}, {
+    'id': 2,
+    'teamId1': 2,
+    'teamId2': 1,
+    'gameDate': datetime(2016, 6, 20),
+}, {
+    'id': 3,
+    'teamId1': 2,
+    'teamId2': 3,
+    'gameDate': datetime(2016, 6, 25),
+}]
+results = {
+    1: {
+        'teamId1': 2,
+        'teamId2': 1,
+        'winner': 1,
+    },
+    3: {
+        'teamId1': 2,
+        'teamId2': 1,
+        'winner': 1,
+    }
+}
+activeUser = 1
+teams = {
+    1: {
+        'teamName': 'France',
+        'crest': 'path/to/crest',
+    },
+    2: {
+        'teamName': 'Italy',
+        'crest': 'path/to/crest',
+    },
+    3: {
+        'teamName': 'Germany',
+        'crest': 'path/to/crest',
+    }
+}
---
+++ main.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+from functools import reduce
+
+from demo_data import *
+
+
+def compute_scores(results):
+    """ return dict with userId and its score """
+    return {
+        id: reduce(
+            compute_user_score,
+            [p for p in pronos if p['userId'] == id],
+            0
+        ) for id in [1, 2, 3]
+    }
+
+
+def compute_user_score(total, game_prono):
+    """ Compute user pronos and return user score """
+    game_result = results[game_prono['gameId']]
+    if game_prono['winner'] != game_result['winner']:
+        return total
+    elif game_prono['teamId1'] == game_result['teamId1'] \
+            and game_prono['teamId2'] == game_result['teamId2']:
+        return total + 3
+    return total + 1
+
+
+def fetch_next_games():
+    """ Fetch games for the next five days """
+    today = datetime(2016, 6, 8)
+    in_five_days = today + timedelta(days=15)
+    return [game for game in games
+            if (game['gameDate'] > today and game['gameDate'] <= in_five_days)]
+
+
+def add_prono(new_prono):
+    """ Simply add a prono to the list """
+    pronos.append(new_prono)
+
+
+if __name__ == '__main__':
+    print(compute_scores(results))
+    print('*******')
+    print('*******')
+    print(fetch_next_games())
+    print('*******')
+    print('*******')
+    print('Adding a prono for Player 3')
+    add_prono({'id': 6, 'gameId': 3, 'userId': 3, 'teamId1': 2,
+                        'teamId2': 0, 'winner': 1})
+    print(compute_scores(results))
